1f2895a69df8addaaf84f2144c3a2d4e8772eec4
Osama <osamarafi229@gmail.com>
2026-03-18 10:36:01 +0000

    Added Product component

---
+++ src/Product.js
@@ -0,0 +1,11 @@
+function Product() {
+  return (
+    <div>
+      <h2>Product Name</h2>
+      <p>Price: $100</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+}
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import Product from "./components/Product";
+
 function App() {
   return (
     <div>
       <h1>Our Products</h1>
+      <Product />
+      <Product />
     </div>
   );
 }
