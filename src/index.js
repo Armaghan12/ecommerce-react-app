6c60293fa6f5b22a42b4e643de7ecb754047cd06
Osama <osamarafi229@gmail.com>
2026-03-18 11:04:50 +0000

    Added Cart Functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Product from "./components/Product";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
     <div>
       <h1>Our Products</h1>
 
-      <Product name="iPhone" price={1000} />
-      <Product name="Laptop" price={2000} />
+      <Product name="iPhone" price={1000} addToCart={addToCart} />
+      <Product name="Laptop" price={2000} addToCart={addToCart} />
 
+      <h2>Cart Items: {cart.length}</h2>
     </div>
   );
 }
